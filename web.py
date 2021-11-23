e92d5d8459195ea68365e255a8329be779c72dd6
Pradeesh333 <pradeesh.3333@gmail.com>
2021-11-23 20:05:58 +0530

    web

---
+++ web.py
@@ -0,0 +1,25 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>My webserver</title>
+</head>
+<body>
+<h1>Pradeesh</h1>
+21500189 <br>
+AIML
+</body>
+</html>
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',1111)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
